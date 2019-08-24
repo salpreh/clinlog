f85238e88c77a2fefc00244dd784555455a31654
salpreh <salva.perez46@gmail.com>
2019-08-25 01:06:48 +0200

    Logger print methods renamed

--- test/log_previews.py
+++ test/log_previews.py
@@ -13,52 +13,52 @@ def sample_call():
     log.error_tag = '[ERROR]: '
 
     # Print error styled message
-    log.print_error('Unable to access config file', bold=True)
+    log.error('Unable to access config file', bold=True)
 
     # Print warning highlighted and bold
-    log.print_warn('The execution will continue with default configuration', None, True, True)
+    log.warn('The execution will continue with default configuration', None, True, True)
 
     # Confirmation log with provided tag
-    log.print_confirm('Execution completed', '[SUCCESS]')
+    log.confirm('Execution completed', '[SUCCESS]')
 
 
 def error_tests(message, log):
     log.error_tag = '[BAD CALL] '
-    log.print_error(message, '')
-    log.print_error(message, '[ERROR] ', True)
-    log.print_error(message, None, False, True)
-    log.print_error(message, '[IMPORTANT] ', True, True)
+    log.error(message, '')
+    log.error(message, '[ERROR] ', True)
+    log.error(message, None, False, True)
+    log.error(message, '[IMPORTANT] ', True, True)
 
 
 def warn_tests(message, log):
     log.warn_tag = '[WARNING] '
-    log.print_warn(message, '')
-    log.print_warn(message, '[WARN] ', True)
-    log.print_warn(message, None, False, True)
-    log.print_warn(message, '[ATTENTION] ', True, True)
+    log.warn(message, '')
+    log.warn(message, '[WARN] ', True)
+    log.warn(message, None, False, True)
+    log.warn(message, '[ATTENTION] ', True, True)
 
 
 def info_tests(message, log):
     log.info_tag = '[INFO] '
-    log.print_info(message, '')
-    log.print_info(message, None, True)
-    log.print_info(message, 'Remember: ', False, True)
-    log.print_info(message, 'HEY: ', True, True)
+    log.info(message, '')
+    log.info(message, None, True)
+    log.info(message, 'Remember: ', False, True)
+    log.info(message, 'HEY: ', True, True)
 
 
 def confirm_tests(message, log):
-    log.print_confirm(message)
-    log.print_confirm(message, '[OK] ', True)
-    log.print_confirm(message, '[ACCEPTED] ', False, True)
-    log.print_confirm(message, '[CONFIRMED] ', True, True)
+    log.confirm(message)
+    log.confirm(message, '[OK] ', True)
+    log.confirm(message, '[ACCEPTED] ', False, True)
+    log.confirm(message, '[CONFIRMED] ', True, True)
 
 
-def normal_print_tests(message, log):
-    log.norm_tag = '[REGULAR] '
-    log.print_norm(message, '')
-    log.print_norm(message, None, True)
-    log.print_norm(message, '', False, True)
-    log.print_norm(message, '', True, True)
+def normal_tests(message, log):
+    log.tag = '[REGULAR] '
+    log.print(message, '')
+    log.print(message, None, True)
+    log.print(message, '', False, True)
+    log.print(message, '', True, True)
 
 
 if __name__ == '__main__':
@@ -67,30 +67,30 @@ if __name__ == '__main__':
 
     # Error preview
     msg = 'Notice this error!'
-    log.print_norm('\nError messages', bold=True)
+    log.print('\nError messages', bold=True)
     print('='*len(msg))
     error_tests(msg, log)
 
     # Warn preview
     msg = 'Keep in mind this'
-    log.print_norm('\nWarning messages', bold=True)
+    log.print('\nWarning messages', bold=True)
     print('='*len(msg))
     warn_tests(msg, log)
 
     # Info preview
     msg = 'You are in the right way'
-    log.print_norm('\nInfo messages', bold=True)
+    log.print('\nInfo messages', bold=True)
     print('='*len(msg))
     info_tests(msg, log)
 
     # Confirm preview
     msg = 'That was absolutely right'
-    log.print_norm('\nConfirmation messages', bold=True)
+    log.print('\nConfirmation messages', bold=True)
     print('='*len(msg))
     confirm_tests(msg, log)
 
     # Normal style preview
     msg = 'Pretty regular message'
-    log.print_norm('\nNomal style messages', bold=True)
+    log.print('\nNomal style messages', bold=True)
     print('='*len(msg))
-    normal_print_tests(msg, log)
+    normal_tests(msg, log)
